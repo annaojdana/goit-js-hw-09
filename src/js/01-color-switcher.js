271e828817dd6641c4997622d138e8f4dfb0f3a3
Anna Ojdana <anna.ojdana@gmail.com>
2022-06-03 13:16:35 +0200

    task01 - ready

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const start = document.querySelector("[data-start]");
+const stop = document.querySelector("[data-stop]");
+const body = document.querySelector("body");
+stop.disabled = true;
 
+start.addEventListener("click", handleStart);
+stop.addEventListener("click", handleStop);
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
+function handleStart() {
+  start.disabled = true;
+  stop.disabled = false;
+    changeColor = setInterval(() => {
+      const bodyColor = getRandomHexColor();
+      body.style.backgroundColor = bodyColor;
+    }, 1000);
+
+};
+function handleStop() {
+  clearInterval(changeColor);
+  start.disabled = false;
+  stop.disabled = true;
+};

6d0c1018ac6db640931b7107acc1609288ac00e7
Anna Ojdana <anna.ojdana@gmail.com>
2022-06-06 12:01:01 +0200

    tas03 - ready

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -11,43 +11,41 @@ function handleSubmit(event) {
   } = event.currentTarget;
   let delayTotal = Number(delay.value);
   if (delay.value >= 0 && step.value >= 0 && amount.value > 0) {
-    setTimeout(createPromise(1, delayTotal),delayTotal);
+    createPromise(1, delay.value)
+    .then(({ position, delay }) => {
+   Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+    .catch(({ position, delay }) => {
+    Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
     for (let i = 2; i <= amount.value; i++) {
       delayTotal += Number(step.value);
-      setTimeout(createPromise(i, delayTotal), delayTotal);
+      createPromise(i, delayTotal)
+      .then(({ position, delay }) => {
+   Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
   }
   }
   else {
     Notiflix.Notify.failure(`❌ select values ​​above 0 `);
   }
-
-
-//   createPromise(1, delay)
-//    .then(({ position, delay }) => {
-//    Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-//   })
-//   .catch(({ position, delay }) => {
-//     Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
-//   });
-// for (let i = 0; i < amount.value; i++) {
-//   createPromise(i, delay.value)
-//   .then(({ position, delay }) => {
-//    Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-//   })
-//   .catch(({ position, delay }) => {
-//     Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
-//   });
-
-
   event.currentTarget.reset();
-}
+}; 
+
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-    Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  } else {
-    // Reject
-    Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
-  }
+  return new Promise((resolve, reject) =>
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        // Fulfill
+        resolve({position,delay});
+      } else {
+        // Reject
+        reject({position,delay});
+      }
+    }, delay))
 }
